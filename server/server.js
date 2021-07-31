ad1a8a234a2c7a986d316a19629ba9690b0a2101
Faisal <saleemfs27@gmail.com>
2021-08-01 00:02:44 +0200

    Setup base server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  res.status(200).send('bruh')
+})
+
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`)
+})
