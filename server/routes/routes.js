e03bfe1ecbf2864137997c551b80de79a59c5e4f
Faisal <saleemfs27@gmail.com>
2021-08-01 12:03:56 +0200

    Added post and get routes

---
+++ server/routes/routes.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import Message from '../models/Message.js'
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.status(200).send('bruh')
+})
+
+router.post('/messages/new', async (req, res) => {
+  const dbMessage = req.body
+  const message = await Message.create(dbMessage)
+  if (!message) {
+    console.log('Error')
+  } else {
+    res.send(message)
+  }
+})
+
+router.get('/messages/sync', async (req, res) => {
+  const messages = await Message.find()
+  if (!messages) {
+    console.log('Error')
+  } else {
+    res.send(messages)
+  }
+})
+
+export default router
--- server/server.js
+++ server/server.js
@@ -1,11 +1,40 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import Pusher from 'pusher'
+import router from './routes/routes.js'
 
+// env config
+dotenv.config()
+
+// app config
 const app = express()
+app.use(express.json())
+app.use('', router)
+
+const pusher = new Pusher({
+  appId: '1243528',
+  key: '66621e983b0debdfab47',
+  secret: process.env.PUSHER_SECRET,
+  cluster: 'ap2',
+  useTLS: true
+})
+
+// port
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.status(200).send('bruh')
-})
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  },
+  () => {
+    console.log('Connected to db')
+  }
+)
 
 app.listen(port, () => {
   console.log(`Server running at port ${port}`)
